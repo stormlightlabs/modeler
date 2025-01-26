aa2a6101392f3818d1859ef73d654c2626e0158a
Owais Jamil <desertthunder.dev@gmail.com>
2025-01-26 02:20:59 -0600

    Initial Commit

---
+++ Mental ModelerTests/Mental_ModelerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mental_ModelerTests.swift
+//  Mental ModelerTests
+//
+//  Created by Owais Jamil on 1/26/25.
+//
+
+import Testing
+@testable import Mental_Modeler
+
+struct Mental_ModelerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
